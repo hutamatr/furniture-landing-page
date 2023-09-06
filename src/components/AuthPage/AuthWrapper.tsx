c20d125f75e0181430b3b136210803644eb0b6d1
hutamatr <hutamatr@gmail.com>
2023-09-06 20:17:54 +0700

    refactor: refactor auth-wrapper component

--- src/components/AuthPage/AuthWrapper.tsx
+++ src/components/AuthPage/AuthWrapper.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { useLocation } from 'react-router-dom';
 
 import authBg from '@assets/image/pattern.webp';
 
@@ -6,40 +7,24 @@ interface IAuthWrapperProps {
   children: React.ReactNode;
   authType: 'Sign In' | 'Sign Up';
   authDescription: string;
+  onUseDemoAccount?: () => void;
 }
 
 export default function AuthWrapper({
   children,
   authType,
   authDescription,
+  onUseDemoAccount,
 }: IAuthWrapperProps) {
+  const pathname = useLocation().pathname;
+
   return (
     <>
-      <section
-        className={clsx(
-          'col-start-1 col-end-13 grid place-items-center',
-          'lg:col-end-6'
-        )}
-      >
-        <div
-          className={clsx(
-            'mx-auto flex w-full flex-col gap-y-4',
-            'md:max-w-sm'
-          )}
-        >
-          <div className='flex flex-col items-start gap-y-2'>
-            <h1 className={clsx('text-2xl font-semibold', 'md:text-3xl')}>
-              {authType}
-            </h1>
-            <p>{authDescription}</p>
-          </div>
-          {children}
-        </div>
-      </section>
       <figure
         className={clsx(
           'hidden',
-          'lg:col-start-6 lg:col-end-13 lg:grid lg:w-full lg:place-items-center lg:overflow-hidden lg:py-4'
+          'lg:col-start-1 lg:col-end-8 lg:grid lg:w-full lg:place-items-center lg:overflow-hidden lg:py-4'
+          // '2xl:col-start-1'
         )}
       >
         <img
@@ -49,6 +34,45 @@ export default function AuthWrapper({
           className={clsx('w-full rounded-3xl', 'md:h-full', '2xl:h-[32rem]')}
         />
       </figure>
+      <section
+        className={clsx(
+          'col-start-1 col-end-13 grid place-items-center',
+          'lg:col-start-8 lg:col-end-13'
+          // '2xl:col-start-6'
+        )}
+      >
+        <div
+          className={clsx(
+            'mx-auto flex w-full flex-col gap-y-4',
+            'md:max-w-sm'
+          )}
+        >
+          <div className='flex flex-col items-start gap-y-2'>
+            <h1
+              className={clsx(
+                'font-neutral text-2xl font-semibold text-custom-black',
+                'md:text-3xl'
+              )}
+            >
+              {authType}
+            </h1>
+            <div className='flex w-full items-center justify-between'>
+              <p className={clsx('text-xs text-custom-black', 'md:text-sm')}>
+                {authDescription}
+              </p>
+              {pathname === '/login' && (
+                <button
+                  className='text-xs text-custom-black underline hover:text-custom-black/80'
+                  onClick={onUseDemoAccount}
+                >
+                  demo account
+                </button>
+              )}
+            </div>
+          </div>
+          {children}
+        </div>
+      </section>
     </>
   );
 }
