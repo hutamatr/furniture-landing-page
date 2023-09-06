890163d6bb83f29c6905232a3d3bba9036461c6b
hutamatr <hutamatr@gmail.com>
2023-09-06 20:24:05 +0700

    feat: create initial protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 
+import RequireAuth from '@components/AuthPage/RequireAuth';
 import Layout from '@components/Layout/Layout';
 
-import LoginPage from '@pages/Login.page';
-
-import './App.css';
+import HomePage from '@pages/Home.page';
+import SignInPage from '@pages/SignIn.page';
+import SignUpPage from '@pages/SignUp,page';
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path='login' element={<LoginPage />} />
+        <Route path='login' element={<SignInPage />} />
+        <Route path='register' element={<SignUpPage />} />
+        <Route element={<RequireAuth />}>
+          <Route path='/' element={<HomePage />} />
+        </Route>
       </Route>
     </Routes>
   );
