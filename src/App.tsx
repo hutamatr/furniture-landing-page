ce2789e2e4eabcdf8679cf31d68a83e36eb594cb
hutamatr <hutamatr@gmail.com>
2023-09-08 19:03:39 +0700

    feat: create get user request & use in profile component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import RequireAuth from '@components/AuthPage/RequireAuth';
 import Layout from '@components/Layout/Layout';
 
 import HomePage from '@pages/Home.page';
+import ProfilePage from '@pages/Profile.page';
 import SignInPage from '@pages/SignIn.page';
 import SignUpPage from '@pages/SignUp,page';
 
@@ -14,7 +15,9 @@ export default function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='login' element={<SignInPage />} />
         <Route path='register' element={<SignUpPage />} />
-        <Route element={<RequireAuth />}></Route>
+        <Route element={<RequireAuth />}>
+          <Route path='profile' element={<ProfilePage />} />
+        </Route>
       </Route>
     </Routes>
   );
