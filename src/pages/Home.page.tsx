799dca8bc3b67293b2c82f6c5cbbd8c80a61a53b
hutamatr <hutamatr@gmail.com>
2023-09-08 14:11:26 +0700

    feat: create home-page component & add smooth scroll

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,7 +1,43 @@
+import { motion, Variants } from 'framer-motion';
+import { useEffect } from 'react';
+
+import About from '@components/Home/About';
+import Catalogue from '@components/Home/Catalogue';
+import Contact from '@components/Home/Contact';
+import Hero from '@components/Home/Hero';
+
+const variants: Variants = {
+  initial: { opacity: 0, y: 24 },
+  animate: { opacity: 1, y: 0, transition: { duration: 1 } },
+  exit: { opacity: 0, y: 0 },
+};
+
 export default function HomePage() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-vars
+      const locomotiveScroll = new LocomotiveScroll({
+        lenisOptions: {
+          lerp: 0.05,
+          smoothWheel: true,
+        },
+      });
+    })();
+  }, []);
+
   return (
-    <section>
-      <h1>Home Page</h1>
-    </section>
+    <motion.section
+      className='col-start-1 col-end-13 flex flex-col gap-y-14'
+      variants={variants}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+    >
+      <Hero />
+      <About />
+      <Catalogue />
+      <Contact />
+    </motion.section>
   );
 }
