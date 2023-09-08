72ae70d50fa3164a2e423c554a714de754723e40
hutamatr <hutamatr@gmail.com>
2023-09-08 13:27:29 +0700

    feat: create scroll to top button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useLocation } from 'react-router-dom';
 
 import Navigation from '@components/Navigation/Navigation';
+import ScrollTop from '@components/UI/ScrollTop/ScrollTop';
 
 export default function Layout() {
   const pathname = useLocation().pathname;
@@ -8,12 +9,13 @@ export default function Layout() {
   return (
     <>
       {pathname !== '/login' && pathname !== '/register' && (
-        <header className='sticky top-0 z-50 w-full bg-custom-black'>
+        <header className='fixed top-0 z-[100] w-full bg-custom-white-2'>
           <Navigation />
         </header>
       )}
       <main className='layout grid-12 h-screen gap-x-2'>
         <Outlet />
+        <ScrollTop />
       </main>
     </>
   );
